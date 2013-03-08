5a054f74032cac8366fbbfe031e0a1e1d184ea62
s145377 <s145377@students.lmsd.org>
2013-03-07 21:09:08 -0500

    rendered the pattern layout onto screen

--- Memory Game/src/com/example/memorygame/FullscreenActivity.java
+++ Memory Game/src/com/example/memorygame/FullscreenActivity.java
@@ -19,6 +19,7 @@ public class FullscreenActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        setContentView(R.layout.pattern);
 		
 	}
 }
