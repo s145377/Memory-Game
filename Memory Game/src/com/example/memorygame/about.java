e7d118c7df506094b8c2a329f3072fb35f429d43
s145377 <s145377@students.lmsd.org>
2013-03-17 20:09:13 -0400

    randomized which name is displayed first on about screen

--- Memory Game/src/com/example/memorygame/about.java
+++ Memory Game/src/com/example/memorygame/about.java
@@ -18,6 +18,22 @@ public class about extends SwarmActivity{
 	    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.about);
         TextView developedby = (TextView) findViewById(R.id.developedby);
+        String programmer1 = "";
+        String programmer2 = "";
+        int p = (int) (Math.random()*100);
+        if(p>50) {
+        	programmer1 = "Jacob Noel";
+        	programmer2 = "Alden Hallak";
+        }
+        else if(p<50) {
+        	programmer1 = "Alden Hallak";
+        	programmer2 = "Jacob Noel";
+        }
+        else {
+        	programmer1 = "a monkey";
+        	programmer2 = "another monkey";
+        }
+        developedby.setText("App envisioned by Jacob Noel and developed by "+programmer1+" and "+programmer2);
 		Typeface bit=Typeface.createFromAsset(getAssets(),"fonts/bus.ttf");
 		developedby.setTypeface(bit);
 		TextView about = (TextView) findViewById(R.id.textbody);
